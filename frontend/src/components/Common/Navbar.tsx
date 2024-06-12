cc5565e02ebd4540c7e79e76e745fb1331b66603
Alex Reynolds <hellport@protonmail.com>
2024-06-12 20:16:16 -0300

    use file picker to select multiple files for mutationFn

--- frontend/src/components/Common/Navbar.tsx
+++ frontend/src/components/Common/Navbar.tsx
@@ -1,7 +1,9 @@
-import { Button, Flex, useDisclosure } from "@chakra-ui/react"
 import { 
-  FaFolder,
-  FaFolderPlus,
+  Button, 
+  Flex, 
+  // useToast,
+} from "@chakra-ui/react"
+import { 
   FaFileAlt,
 } from "react-icons/fa"
 import { ChevronDownIcon } from '@chakra-ui/icons'
@@ -10,13 +12,48 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
-  MenuDivider,
 } from '@chakra-ui/react'
-
-import AddFile from "../Files/AddFile"
+import { useFilePicker } from 'use-file-picker';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { type ApiError, type FileCreate, FilesService } from "../../client"
 
 const Navbar = () => {
-  const addFileModal = useDisclosure()
+  const queryClient = useQueryClient()
+  // const toast = useToast()
+  const mutation = useMutation({
+    mutationFn: (files: File[]) => {
+      const handleFileUpload = async (file: File) => {
+        // send file to storage
+
+        const metadata: FileCreate = {
+          path: file.name
+        };
+
+        // send metadata to our backend
+        return FilesService.createFile({ requestBody: metadata });
+      };
+
+      // process each file in parallel
+      return Promise.all(files.map(file => handleFileUpload(file)));
+    },
+    onSuccess: () => {
+      console.log("success")
+    },
+    onError: (err: ApiError) => {
+      const errDetail = (err.body as any)?.detail || "An unknown error occurred";
+      console.log(errDetail)
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["files"] })
+    },
+  })
+
+  const { openFilePicker } = useFilePicker({
+    accept: "*",
+    onFilesSelected: ({ plainFiles }) => {
+      mutation.mutate(plainFiles)
+    },
+  });
 
   return (
     <>
@@ -29,27 +66,14 @@ const Navbar = () => {
             New
           </MenuButton>
           <MenuList>
-            <MenuItem
-              icon={<FaFolderPlus />}
-            >
-              New Folder
-            </MenuItem>
-            <MenuDivider />
             <MenuItem 
               icon={<FaFileAlt />}
-              onClick={addFileModal.onOpen}
+              onClick={() => {openFilePicker()}}
             >
               File Upload
             </MenuItem>
-            <MenuItem
-              icon={<FaFolder />}
-            >
-              Folder Upload
-            </MenuItem>
           </MenuList>
         </Menu>
-
-        <AddFile isOpen={addFileModal.isOpen} onClose={addFileModal.onClose} />
       </Flex>
     </>
   )
