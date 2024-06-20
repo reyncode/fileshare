840608a54f5de19277aa509bb8692828adce71ad
Alex Reynolds <hellport@protonmail.com>
2024-06-20 08:37:40 -0300

    get aws creds out of the frontend, hardcode creds until a secret manager exists

--- frontend/src/client/aws-config.ts
+++ frontend/src/client/aws-config.ts
@@ -2,9 +2,9 @@ import AWS from "aws-sdk";
 import config from "../config"
 
 const s3 = new AWS.S3({
-    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-    region: process.env.REACT_APP_AWS_REGION,
+    accessKeyId: "access-key-id",
+    secretAccessKey: "secret-access-key",
+    region: "ca-central-1",
     endpoint: config.REACT_APP_STORAGE_URL,
     s3ForcePathStyle: true,
 });
